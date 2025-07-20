ebf73bdf6ccc4a94ca76e47f5f272669c709a3ba
Irgb77 <irgb77@gmail.com>
2025-07-20 13:20:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/keilar-medtech-innovate/', // <-- Línea añadida para GitHub Pages
   server: {
     host: "::",
     port: 8080,
